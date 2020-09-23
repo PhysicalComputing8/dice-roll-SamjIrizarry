c8d0222e234a98980ec1fbc69365ba27667a017a
Trevor Shaw <shawt@genlrn.com>
2020-09-23 11:50:48 -0400

    Teacher Feedback

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ input.onGesture(Gesture.Shake, function () {
     // A variable is a place where your program can store a value. We need a place to store the result of the roll, so I created a variable called roll.
     // Nice work! You correctly changed the minimum and maximum of the numbers that will be generated, so it resembles the way dice behave.
     roll = randint(1, 6)
+    // This is a good start, but I was hoping to see at least some of the advanced project completed by the end of class today.
+    // 
+    // Please let me know if you are having any trouble, so I can help you keep from falling behind.
     basic.showNumber(roll)
 })
 // Code added here will run once when the program starts
